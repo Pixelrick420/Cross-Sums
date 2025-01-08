ad0da0282e97991adb76745ffc83f96235db8cde
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2025-01-08 22:19:50 +0530

    stable 1.1

--- cross_sums.py
+++ cross_sums.py
@@ -1,16 +1,17 @@
 import random
 
 class Board:
-    def __init__ (self, n):
+    def __init__(self, n):
         self.n = n
         self.answer = set()
         self.rowSums = [0] * n
         self.colSums = [0] * n
+        self.solution = set()
         self.matrix = [[0] * n for _ in range(n)]
         for i in range(n):
             for j in range(n):
                 self.matrix[i][j] = random.randint(1, 9)
-                if(random.randint(1, 10)) <= 2:
+                if random.randint(1, 10) <= 2:
                     self.answer.add((i, j))
                     self.rowSums[i] += self.matrix[i][j]
                     self.colSums[j] += self.matrix[i][j]
@@ -22,56 +23,99 @@ class Board:
         for _ in range(n):
             i = rows.pop()
             j = cols.pop()
-            if(i, j) not in self.answer:
+            if (i, j) not in self.answer:
                 self.answer.add((i, j))
                 self.rowSums[i] += self.matrix[i][j]
                 self.colSums[j] += self.matrix[i][j]
 
-
     def display(self):
-        def displayRow(array):
+        print('\n')
+        def displayRow(array, row, isSum = False):
             print('\t', end='')
-            for val in array:
-                print(val, end=(' ' * (6 - len(str(val)))))
-            print()
-        for row in self.matrix:
-            displayRow(row)
-        
-        print(self.rowSums)
-        print(self.colSums)
+            for i in range(len(array)):
+                print(array[i], end=(' ' * (6 - len(str(array[i])))))
+            if not isSum:
+                print('   ', self.rowSums[row])
+            else:
+                print()
+        for i in range(self.n):
+            displayRow(self.matrix[i], i)
+        print()
+        displayRow(self.colSums, 0, True)
+        print('\n')
     
     def sums(self, array, target):
+        if target == 0:
+            return
+            
         out = set()
+        
         def f(curSum, index, sub):
-            if(curSum == target):
+            if curSum == target:
                 out.add(sub)
                 return
-            elif index >= len(array):
+            if index >= len(array):
                 return
-            elif curSum < target:
-                f(curSum + array[index], index + 1, sub + ((array[index], index),) )
-                f(curSum, index + 1, sub)
+            if curSum < target and array[index] != 'X':
+                f(curSum + array[index], index + 1, sub + (index,))
+            f(curSum, index + 1, sub)
+
         f(0, 0, tuple())
         return out
     
     def solve(self):
         total = set(i for i in range(self.n))
-        for i in range(self.n):
-            possible = list(self.sums(self.matrix[i], self.rowSums[i]))
-            cols = set()
-            for solution in possible:
-                cols.update([j for (i, j) in solution])
-            discard = list(total.difference(cols))
-            for col in discard:
-                self.matrix[i][col] = 'X'
-            
+        
+        order = list(range(self.n))
+        random.shuffle(order)
+        for row in order:
+            if self.rowSums[row] > 0:
+                possible = list(self.sums(self.matrix[row], self.rowSums[row]))
+                cols = set()
+
+                if len(possible) == 1:
+                    confirm = [(row,col) for col in possible[0]]
+                    self.solution.update(confirm)
+
+                for solution in possible:
+                    cols.update([col for col in solution])
+                discard = list(total.difference(cols))
+                for col in discard:
+                    self.matrix[row][col] = 'X'
+        
+        transposedMatrix = [[self.matrix[j][i] for j in range(self.n)] for i in range(self.n)]
+        random.shuffle(order)
+        for col in order:
+            if self.colSums[col] > 0:
+                possible = list(self.sums(transposedMatrix[col], self.colSums[col]))
+                rows = set()
+
+                if len(possible) == 1:
+                    confirm = [(row,col) for row in possible[0]]
+                    self.solution.update(confirm)
+
+                for solution in possible:
+                    rows.update([row for row in solution])
+                discard = list(total.difference(rows))
+                for row in discard:
+                    self.matrix[row][col] = 'X'
+
+    def check(self):
+        return self.answer == self.solution
 
 def main():
     board = Board(6)
+    
     board.display()
     board.solve()
+    board.solve()
+    board.solve()
+    board.solve()
+    board.solve()
+    board.solve()
     board.display()
+    print(board.solution)
+    print(board.check())
 
 if __name__ == '__main__':
     main()
-
